b31ee91e1fad9caee7b4ecfea979cc72c372727d
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2024-06-09 14:53:47 -0300

    adding modal notification windows to home-page

--- codigo-fonte/home/script/app.js
+++ codigo-fonte/home/script/app.js
@@ -27,3 +27,15 @@ const novoCardEvento = `
 
 corpo.insertAdjacentHTML('beforeend', novoCardEvento);
 
+
+const botao = document.getElementById('notificacao')
+const modal = document.querySelector('dialog');
+const botaoFechar = document.getElementById('fechar-dialog')
+
+botao.onclick = function () {
+    modal.showModal();
+}
+
+botaoFechar.onclick = function () {
+    modal.close();
+}
