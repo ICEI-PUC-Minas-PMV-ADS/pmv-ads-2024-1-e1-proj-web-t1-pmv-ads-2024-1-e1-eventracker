5c666e467410bf00f22e942b52d5f308945a09c7
Juliana M <134242381+jumonik09@users.noreply.github.com>
2024-06-17 23:49:49 -0300

    alteracoes  java script pagina login  e direcionando para pagina home

--- codigo-fonte/login/login.js
+++ codigo-fonte/login/login.js
@@ -1,22 +1,25 @@
+
 function login(event) {
     event.preventDefault();
     console.log("entrou na funcao login");
 
     const email = document.getElementsByName("email")[0].value
-    console.log("O email ingressado é " + email);
-
     const senha = document.getElementsByName("senha")[0].value
+    console.log("O email ingressado é " + email);
     console.log("A senha ingressada é " + senha);
 
     if (email == "" || senha == "") {
         alert("Os campos email e senha são obrigatórios.");
+    } else if (!temArroba(email)) {
+        alert("Erro, o email não tem @");
     } else {
+        console.log("Deveria agora navegar pro home");
         alert("Login realizado com sucesso!");
         window.location.href = "../home/index.html";
     }
 
-    //limpar campos senha e email aqui
-    document.getElementsByName("email")[0].value = ""
-    document.getElementsByName("senha")[0].value = ""
+}
+function temArroba(dado) {
+    return dado.includes('@')
 }
 
