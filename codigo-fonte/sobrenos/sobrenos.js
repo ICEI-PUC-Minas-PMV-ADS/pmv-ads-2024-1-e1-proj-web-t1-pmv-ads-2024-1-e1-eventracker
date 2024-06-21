35986c18ab6c36dfe6448f735f54818263f329e4
Juliana M <134242381+jumonik09@users.noreply.github.com>
2024-06-20 21:32:54 -0300

    api acrescentado (clima)

--- codigo-fonte/sobrenos/sobrenos.js
+++ codigo-fonte/sobrenos/sobrenos.js
@@ -28,3 +28,34 @@ function goToCadastro() {
     console.log("clicou no botao cadastro");
     window.location.href = "../nova-conta/index.html";
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    //Inicio da funcao
+
+    console.log("Pagina carregada, deveria chamar api de clima agora");
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.hgbrasil.com/weather?key=c83dc503&city_name=Belo%20Horizonte,%20MG', true);
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            let responseJson = JSON.parse(xhr.responseText);
+
+            let temperatura = responseJson.results.temp
+            
+            console.log("Temperatura:", temperatura)
+
+            let tempH1 = document.getElementById("temperature_text") 
+            tempH1.textContent = "Veja a temperatura de Belo Horizonte e escolha seu evento ideal: " + temperatura + "º"
+
+        } else {
+            console.error('Erro na requisição:', xhr.statusText);
+        }
+    };
+    xhr.onerror = function () {
+        console.error('Erro na requisição');
+    };
+    xhr.send();
+
+
+    //Fim da funcao
+});
